1aaaed558d81bc9a3c40a802f7ad588a6a455d32
Andrew Hird <eda@mac-mini-1.local>
2016-10-04 15:35:24 +1300

    user story 2 working

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,13 @@ export const addWord = (word) => {
     word
   }
 }
+
+let nextImageId = 0
+
+export const addImage = (image) => {
+  return {
+    type: 'ADD_IMAGE',
+    id: nextImageId++,
+    image
+  }
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
 import AddWord from './AddWord'
+import AddImage from './AddImage'
 import WordsContainer from '../containers/WordsContainer'
+import ImageContainer from '../containers/ImageContainer'
+
 
 const App = () => (
   <div className='app-container'>
     <WordsContainer />
     <AddWord />
+    <ImageContainer />
+    <AddImage />
   </div>
 )
 
